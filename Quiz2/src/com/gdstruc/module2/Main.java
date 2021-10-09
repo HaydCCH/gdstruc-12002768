cde9c1824278912a2e35174ff214e222f1c4b0b7
HaydnCCH <haydncraig.hawson@benilde.edu.ph>
2021-10-09 18:01:17 +0800

    done

--- Quiz2/src/com/gdstruc/module2/Main.java
+++ Quiz2/src/com/gdstruc/module2/Main.java
@@ -1,36 +1,32 @@
 package com.gdstruc.module2;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        List<linklist> playerList = new ArrayList<>();
-        playerList.add(new linklist(1,"Kirito", 100 ));
-        playerList.add(new linklist(2,"Asuna", 69 ));
-        playerList.add(new linklist(3,"Klein", 59 ));
-        playerList.add(new linklist(4,"Leafa", 60 ));
-        for (linklist p : playerList)
-        {
-            System.out.println(p);
-        }
-         playerList.remove(0);
-        System.out.print("Here is the list with the removed variable" +
-                "");
-        for (linklist p : playerList)
-        {
 
-            System.out.println(p);
-        }
-int count = playerList.size();
-        System.out.print("The number of players are "+count+
-                "" +
-                "        ");
-
-        System.out.println(playerList.contains(new linklist(4,"Leafa",60) ));
-        System.out.println(playerList.indexOf(new linklist(1, "Kirito",100)));
-
-
+        linklist kirito =(new linklist(1,"Kirito", 100 ));
+        linklist asuna =(new linklist(2,"Asuna", 69 ));
+        linklist klien =(new linklist(3,"Klein", 59 ));
+        linklist leafa =(new linklist(4,"Leafa", 60 ));
+        LINKEDLIST newlinkedlist = new LINKEDLIST();
+        newlinkedlist.addtoFront(kirito);
+        newlinkedlist.addtoFront(asuna);
+        newlinkedlist.addtoFront(klien);
+        newlinkedlist.addtoFront(leafa);
+        newlinkedlist.printList();
+System.out.println("There are ");
+newlinkedlist.count();
+System.out.println("Contain");
+        System.out.println(newlinkedlist.contains(kirito,1,"Kirito",100));
+        System.out.println();
+        System.out.println("Index Search ");
+        System.out.println(newlinkedlist.indexof(asuna,2,"Asuna",69));
+        newlinkedlist.deleteFront();
+        System.out.println();
+        System.out.println("Removed front!:");
+        newlinkedlist.printList();
     }
 }

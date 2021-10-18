fade0c845f3916d593b9b5622832aeac39e23ef7
HaydnCCH <haydncraig.hawson@benilde.edu.ph>
2021-10-18 15:49:04 +0800

    Done

--- Quiz3/src/com/gdstruc/module3/Que.java
+++ Quiz3/src/com/gdstruc/module3/Que.java
@@ -1,5 +1,7 @@
 package com.gdstruc.module3;
 
+import java.util.NoSuchElementException;
+
 public class Que {
     private PLayer[] queue;
     private int front;
@@ -18,11 +20,46 @@ public class Que {
             PLayer[] newarray = new PLayer[queue.length * 2];
 System.arraycopy(queue,0,newarray,0,queue.length);
 queue= newarray;
-queue[back]= player;
-back++;
 
 
+        }
+        queue[back]= player;
+        back++;
+    }
+    public PLayer remove()
+    {
+if (size() == 0)
+{
+    throw new  NoSuchElementException();
+}
+PLayer removedplayer = queue[front];
+queue[front]= null;
+front++;
+if (size() == 0)
+{
+    front= 0;
+    back=0;
+}
+return removedplayer;
+    }
+    public PLayer peek()
+    {
+        if (size()==0)
+        {
+            throw new NoSuchElementException();
+        }
+        return queue[front];
+    }
 
+    public int size()
+    {
+        return back-front;
+    }
+    public void printquueue()
+    {
+        for (int i = front;i< back; i++)
+        {
+            System.out.println(queue[i]);
         }
     }
 }

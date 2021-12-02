c85fe791a117885b135b692f566fcc2b6781bd5c
HaydnCCH <haydncraig.hawson@benilde.edu.ph>
2021-12-02 21:23:09 +0800

    done

---
+++ QUIZ  5/src/com/gdstruc/module06/Main.java
@@ -0,0 +1,39 @@
+package com.gdstruc.module06;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int c, n, search, array[];
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("how much do you want in your array");
+        n = in.nextInt();
+
+        array = new int[n];
+        System.out.println("Enter those " + n + " numbers");
+        for (c = 0; c <n; c++)
+        {
+
+
+
+            array[c] = in.nextInt();
+
+
+        }
+
+        System.out.println("Enter value to find");
+        search = in.nextInt();
+        for (c = 0; c <n; c++)
+        {
+            if (array[c] == search)
+            {
+                System.out.println(search + " is present at location " + (c + 1) + ".");
+                break;
+            }
+
+        }
+        if (c == n)  /* Element to search isn't present */
+            System.out.println(search + " isn't present in array.");
+    }
+}
